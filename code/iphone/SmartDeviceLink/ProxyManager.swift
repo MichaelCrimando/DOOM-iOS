190f437288ab91bef7a68854a132f4e09265e750
Michael Crimando <crimandomichael@yahoo.com>
2018-08-14 22:01:08 -0400

    Stopped at SDL Manager Step 9

--- code/iphone/SmartDeviceLink/ProxyManager.swift
+++ code/iphone/SmartDeviceLink/ProxyManager.swift
@@ -6,11 +6,39 @@
 //
 
 import Foundation
+import SmartDeviceLink
+
 class ProxyManager: NSObject {
+    // Manager
+    fileprivate var sdlManager: SDLManager!
+    
     // Singleton
     static let sharedManager = ProxyManager()
     
     private override init() {
         super.init()
+        
+        let lifecycleConfiguration = SDLLifecycleConfiguration(appName:"Doom", appId: "666")
+        lifecycleConfiguration.shortAppName = "Doom"
+        
+        let appIcon = SDLArtwork(image: #imageLiteral(resourceName: "Doom App Icon"), name: "Doom", persistent: true, as: .PNG)
+        lifecycleConfiguration.appIcon = appIcon
+        SDLLockScreenConfiguration.enabled()
+        SDLLogConfiguration.default()
+        
+        lifecycleConfiguration.appType = .navigation
+        let frameRate:Int = 30
+        let averageBitRate:Int = 1000000
+        
+        
+        let configuration = SDLConfiguration(lifecycle: lifecycleConfiguration, lockScreen: .enabled(), logging: .default())
+        
+        //setup Streaming configuration
+        let videoEncoderSettings = [kVTCompressionPropertyKey_ExpectedFrameRate as String: frameRate, kVTCompressionPropertyKey_AverageBitRate as String: averageBitRate]
+        
+        //TODO: Implement Secure streaming
+        //let streamingConfig = SDLStreamingMediaConfiguration(securityManagers: [FMCSecurityManager.self], encryptionFlag: self.encryptionSetting, videoSettings: videoEncoderSettings, dataSource: self, rootViewController: self.sdlViewController)
+        
+        sdlManager = SDLManager(configuration: configuration, delegate: self)
     }
 }
