b836a043c08760d0a894a99f35de12fb08d2e47c
Michael Crimando <crimandomichael@yahoo.com>
2018-08-15 11:25:26 -0400

    SDL Setup as media app complete, registers

--- code/iphone/SmartDeviceLink/ProxyManager.swift
+++ code/iphone/SmartDeviceLink/ProxyManager.swift
@@ -10,6 +10,8 @@ import SmartDeviceLink
 
 class ProxyManager: NSObject {
     // Manager
+    private let appName = "Doom"
+    private let appId = "666"
     fileprivate var sdlManager: SDLManager!
     
     // Singleton
@@ -18,15 +20,19 @@ class ProxyManager: NSObject {
     private override init() {
         super.init()
         
-        let lifecycleConfiguration = SDLLifecycleConfiguration(appName:"Doom", appId: "666")
-        lifecycleConfiguration.shortAppName = "Doom"
+        let lifecycleConfiguration = SDLLifecycleConfiguration(appName:appName, appId: appId)
+        lifecycleConfiguration.shortAppName = appName
         
-        let appIcon = SDLArtwork(image: #imageLiteral(resourceName: "Doom App Icon"), name: "Doom", persistent: true, as: .PNG)
-        lifecycleConfiguration.appIcon = appIcon
+        if let appImage = UIImage(named: "Doom App Icon") {
+            let appIcon = SDLArtwork(image: appImage, name: "Doom", persistent: true, as: .JPG)
+            lifecycleConfiguration.appIcon = appIcon
+        }
+        
+        
         SDLLockScreenConfiguration.enabled()
         SDLLogConfiguration.default()
         
-        lifecycleConfiguration.appType = .navigation
+        lifecycleConfiguration.appType = .media
         let frameRate:Int = 30
         let averageBitRate:Int = 1000000
         
@@ -37,8 +43,29 @@ class ProxyManager: NSObject {
         let videoEncoderSettings = [kVTCompressionPropertyKey_ExpectedFrameRate as String: frameRate, kVTCompressionPropertyKey_AverageBitRate as String: averageBitRate]
         
         //TODO: Implement Secure streaming
-        //let streamingConfig = SDLStreamingMediaConfiguration(securityManagers: [FMCSecurityManager.self], encryptionFlag: self.encryptionSetting, videoSettings: videoEncoderSettings, dataSource: self, rootViewController: self.sdlViewController)
+        //let streamingConfig = SDLStreamingMediaConfiguration(securityManagers: [FMCSecurityManager.self], encryptionFlag: self.encryptionSetting, videoSettings: videoEncoderSettings, dataSource: self, rootViewController: self.sdlViewController)
         
-        sdlManager = SDLManager(configuration: configuration, delegate: self)
+        sdlManager = SDLManager(configuration: configuration, delegate: nil)
+        
+        
+    }
+    func connect() {
+        // Start watching for a connection with a SDL Core
+        sdlManager.start { (success, error) in
+            if success {
+                // Your app has successfully connected with the SDL Core
+            }
+        }
     }
 }
+
+
+//extension ProxyManager: SDLManagerDelegate {
+//    func managerDidDisconnect() {
+//        print("Manager disconnected!")
+//    }
+//
+//    func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel){
+//        print("Doom went from HMI level \(oldLevel) to HMI level \(newLevel)")
+//    }
+//}
