e80400bc4e93ae2784a8a84f25ff70e5d913e67e
Michael Crimando <crimandomichael@yahoo.com>
2018-08-27 14:25:47 -0400

    Ok actually works now

--- code/iphone/SmartDeviceLink/ProxyManager.swift
+++ code/iphone/SmartDeviceLink/ProxyManager.swift
@@ -14,17 +14,20 @@ class ProxyManager: NSObject, SDLStreamingMediaManagerDataSource {
     private let appId = "666"
     var isVideoStreamStarted: Bool = false
     var sdlViewController:UIViewController? = blankViewController()
+    var subscribeVehicleData : SDLSubscribeVehicleData
     
     // Singleton
     static let sharedManager = ProxyManager()
     
     private override init() {
+        subscribeVehicleData = SDLSubscribeVehicleData()
         super.init()
+        
         
         let lifecycleConfiguration = SDLLifecycleConfiguration(appName:appName, appId: appId)
         lifecycleConfiguration.shortAppName = appName
         
-
+        
         let appIcon = SDLArtwork(image: #imageLiteral(resourceName: "DOOM_72.png"), name: "Doom", persistent: true, as: .JPG)
         lifecycleConfiguration.appIcon = appIcon
         
@@ -49,7 +52,50 @@ class ProxyManager: NSObject, SDLStreamingMediaManagerDataSource {
         
         self.isVideoStreamStarted = true
         
+        NotificationCenter.default.addObserver(self, selector: #selector(vehicleDataAvailable(_:)), name: .SDLDidReceiveVehicleData, object: nil)
+        
+        
+        subscribeVehicleData.bodyInformation = true
+        
+        sdlManager.send(request: subscribeVehicleData) { (request, response, error) in
+            guard let response = response as? SDLSubscribeVehicleDataResponse else { return }
+            
+            guard response.success.boolValue == true else {
+                if response.resultCode == .disallowed {
+                    // Not allowed to register for this vehicle data.
+                } else if response.resultCode == .userDisallowed {
+                    // User disabled the ability to give you this vehicle data
+                } else if response.resultCode == .ignored {
+                    if let bodyData = response.bodyInformation {
+                        if bodyData.resultCode == .dataAlreadySubscribed {
+                            // You have access to this data item, and you are already subscribed to this item so we are ignoring.
+                        } else if bodyData.resultCode == .vehicleDataNotAvailable {
+                            // You have access to this data item, but the vehicle you are connected to does not provide it.
+                        } else {
+                            print("Unknown reason for being ignored: \(bodyData.resultCode)")
+                        }
+                    } else {
+                        print("Unknown reason for being ignored: \(String(describing: response.info))")
+                    }
+                } else if let error = error {
+                    print("Encountered Error sending SubscribeVehicleData: \(error)")
+                }
+                return
+            }
+            
+            // Successfully subscribed
+        }
     }
+    
+    func vehicleDataAvailable(_ notification: SDLRPCNotificationNotification) {
+        guard let onVehicleData = notification.notification as? SDLOnVehicleData else {
+            return
+        }
+        
+        let bodyData = onVehicleData.bodyInformation
+        
+    }
+    
     func connect() {
         // Start watching for a connection with a SDL Core
         sdlManager.start { (success, error) in
@@ -70,15 +116,15 @@ class ProxyManager: NSObject, SDLStreamingMediaManagerDataSource {
         return [imageRes]
     }
     
-
+    
     public var sdlManager: SDLManager!
     var streamManager: SDLStreamingMediaManager? {
-//        if !isConnected {
-//            return nil
-//        }
+        //        if !isConnected {
+        //            return nil
+        //        }
         return self.sdlManager.streamManager
     }
-
+    
 }
 
 
@@ -88,7 +134,7 @@ extension ProxyManager: SDLManagerDelegate {
     func managerDidDisconnect() {
         print("Manager disconnected!")
     }
-
+    
     func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel) {
         print("Went from HMI level \(oldLevel) to HMI level \(newLevel)")
     }
