785331a32ff6d50802870b67a20f0bd31c8d9e30
Michael Crimando <crimandomichael@yahoo.com>
2019-10-29 12:09:26 -0400

    Should work

--- code/SWIFTY.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SWIFTY.swift
-//  DOOM
-//
-//  Created by Michael Crimando on 10/29/19.
-//
-
-import Foundation
--- code/iphone/SmartDeviceLink/FInalDOOM-tvOS-Bridging-Header.h
+++ code/iphone/SmartDeviceLink/FInalDOOM-tvOS-Bridging-Header.h
@@ -1,1 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//#import "../FMCSecurity/FMCSecurity/common/FMCSecurityManager.h"
+//#import "../FMCSecurity/FMCSecurity/common/FMCSecurityConstants.h"
+
+#import"/Users/michaelcrimando/GitHub/DOOM-iOS/code/iphone/FMCSecurity/FMCSecurity/common/FMCSecurityConstants.h"
+#import "/Users/michaelcrimando/GitHub/DOOM-iOS/code/iphone/FMCSecurity/FMCSecurity/common/FMCSecurityManager.h"
+
+#import "VehicleController.h"
 
